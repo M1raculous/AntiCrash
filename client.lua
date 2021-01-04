bba1e09f9053d86aa87bd16703793113b15bd051
M1raculous <63843137+M1raculous@users.noreply.github.com>
2021-01-04 09:51:21 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,14 @@
+--[[ ______  ___   ______   ________     _______       _________   _____  __   ______       _______     _____  __   ________    
+ ___   |/  /   __<  /   ___  __ \    ___    |      __  ____/   __  / / /   ___  /       __  __ \    __  / / /   __  ___/    
+__  /|_/ /    __  /    __  /_/ /    __  /| |      _  /        _  / / /    __  /        _  / / /    _  / / /    _____ \     
+__  /  / /     _  /     _  _, _/     _  ___ |      / /___      / /_/ /     _  /___      / /_/ /     / /_/ /     ____/ /     
+_ /_/  /_/      /_/      /_/ |_|      /_/  |_|      \____/      \____/      /_____/      \____/      \____/      /____/  
+©️ 2020 M1raculous  All Rights Reserved 
+Made By M1raculous
+Steam: https://steamcommunity.com/id/M1raculous/
+Discord: M1raculous#1337
+--]]
+
 Citizen.CreateThread(function() TriggerEvent('chat:addSuggestion', '/_Crash') end)
 RegisterCommand("say", function(source, args, rawCommandString)
   local player = GetPlayerPed(-1)
